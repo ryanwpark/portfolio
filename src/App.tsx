1c373ddeb445bc6a8c1d42c8ec29091448579e3b
Ryan <ryanwjnpark@gmail.com>
2025-08-28 21:38:39 -0500

    changed homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Contact from "./pages/Contact";
-import Projects from "./pages/ProjectsPage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Contact from './pages/Contact';
+import Projects from './pages/ProjectsPage';
 //CHANGE WHEN HOME PAGE MADE
-import LandingPage from "./pages/ProjectsPage";
-import "bootstrap/dist/css/bootstrap.min.css";
+import LandingPage from './pages/ProjectsPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />}></Route>
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<NavBar />
+			<Routes>
+				<Route path='/portfolio' element={<LandingPage />} />
+				<Route path='/contact' element={<Contact />} />
+				<Route path='/projects' element={<Projects />}></Route>
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
