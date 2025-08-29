d43462cd146740f0d95cc4be1c1e3dd3bf68c050
Ryan <ryanwjnpark@gmail.com>
2025-08-29 03:31:17 -0500

    added hash routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import ContactPage from "./pages/ContactPage";
-import ProjectsPage from "./pages/ProjectsPage";
-import LandingPage from "./pages/LandingPage";
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import ContactPage from './pages/ContactPage';
+import ProjectsPage from './pages/ProjectsPage';
+import LandingPage from './pages/LandingPage';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<NavBar />
+			<Routes>
+				<Route path='/' element={<LandingPage />} />
+				<Route path='/contact' element={<ContactPage />} />
+				<Route path='/projects' element={<ProjectsPage />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
