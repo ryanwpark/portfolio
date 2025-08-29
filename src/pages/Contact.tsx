43f5611dab566f596c18681dcd1bfece3e3f3ad4
Ryan <ryanwjnpark@gmail.com>
2025-08-28 20:32:17 -0500

    contact page complete

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,142 +1,203 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 import {
-  Form,
-  FloatingLabel,
-  Button,
-  Row,
-  Col,
-  Container,
-  Modal,
-} from "react-bootstrap";
+	Form,
+	FloatingLabel,
+	Button,
+	Row,
+	Col,
+	Container,
+	Spinner,
+} from 'react-bootstrap';
+import { motion } from 'motion/react';
+import * as formik from 'formik';
+import * as yup from 'yup';
 
-function MyVerticallyCenteredModal(props: React.ComponentProps<typeof Modal>) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Body>
-        <p>
-          Thank you for the message and I will get back to you as soon as
-          possible.
-        </p>
-      </Modal.Body>
-    </Modal>
-  );
+function isValidEmail(email: string) {
+	const emailRegex = /^[\w.-]+@[\w.-]+\.\w+$/;
+	return emailRegex.test(email);
 }
 
 const Contact: React.FC = () => {
-  const navigate = useNavigate();
-  const [validated, setValidated] = useState(false);
-  const [modalShow, setModalShow] = useState(false);
-  const [pendingSubmit, setPendingSubmit] = useState(false);
+	const [pendingSubmit, setPendingSubmit] = useState(false);
+	const [submitMessage, setSubmitMessage] = useState('');
+	const { Formik } = formik;
+	const schema = yup.object().shape({
+		emailfullname: yup.string().required(),
+		emailsender: yup
+			.string()
+			.email()
+			.required()
+			.test('is-valid-email', 'Invalid email address', (value) =>
+				isValidEmail(value || '')
+			),
+		emailmsg: yup.string().required(),
+	});
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    if (!form.checkValidity()) {
-      event.stopPropagation();
-      setValidated(true);
-      return;
-    }
-    setValidated(true);
-    setPendingSubmit(true);
-    setModalShow(true);
-  };
+	return (
+		<Formik
+			validationSchema={schema}
+			validateOnChange={false}
+			onSubmit={(values, { resetForm }) => {
+				setPendingSubmit(true);
+				setSubmitMessage('');
+				fetch('https://gmailsenderapi.onrender.com/send-email', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'Application/JSON',
+					},
+					body: JSON.stringify(values),
+				})
+					.then((res) => {
+						setTimeout(() => {
+							setPendingSubmit(false);
+							if (res.ok) {
+								setSubmitMessage('Message sent successfully!');
+								resetForm();
+							} else {
+								setSubmitMessage('Error sending message.');
+							}
+						}, 500);
+						return res.json();
+					})
+					.catch(() => {
+						setTimeout(() => {
+							setPendingSubmit(false);
+							setSubmitMessage('Error sending message.');
+						}, 500);
+					});
+			}}
+			initialValues={{
+				emailsender: '',
+				emailmsg: '',
+				emailfullname: '',
+			}}>
+			{({ handleSubmit, handleChange, values, errors }) => (
+				<motion.div
+					initial={{ opacity: 0, y: 24 }}
+					whileInView={{ opacity: 1, y: 0 }}
+					viewport={{ once: false, amount: 0.2 }}
+					transition={{ duration: 1, delay: 0.7 }}>
+					<Container>
+						<div className='mt-5'>
+							{submitMessage && (
+								<p
+									className={
+										submitMessage.includes('success')
+											? 'text-success text-center'
+											: 'text-danger text-center'
+									}>
+									{submitMessage}
+								</p>
+							)}
+						</div>
+						<h1 className='text text-black text-center mt-5 mb-5'>
+							Thanks for taking the time to reach out. How can I
+							help you today?
+						</h1>
+						<Form noValidate onSubmit={handleSubmit}>
+							<Row className='justify-content-center'>
+								<Col xs lg='3'>
+									<Form.Group
+										className='mb-3'
+										controlId='validationFormik01'>
+										<FloatingLabel
+											controlId='floatingname'
+											className='mb-3'
+											label='Name'>
+											<Form.Control
+												type='text'
+												name='emailfullname'
+												placeholder='name'
+												value={values.emailfullname}
+												onChange={handleChange}
+												isInvalid={
+													!!errors.emailfullname
+												}
+											/>
+											<Form.Control.Feedback type='invalid'>
+												Please enter a name.
+											</Form.Control.Feedback>
+										</FloatingLabel>
+									</Form.Group>
+								</Col>
+								<Col xs lg='3'>
+									<Form.Group
+										className='mb-3'
+										controlId='validationFormik02'>
+										<FloatingLabel
+											controlId='floatingemail'
+											label='Email address'
+											className='mb-3'>
+											<Form.Control
+												required
+												type='email'
+												name='emailsender'
+												placeholder='name@example.com'
+												value={values.emailsender}
+												onChange={handleChange}
+												isInvalid={!!errors.emailsender}
+											/>
+											<Form.Control.Feedback type='invalid'>
+												Please enter a valid email.
+											</Form.Control.Feedback>
+										</FloatingLabel>
+									</Form.Group>
+								</Col>
+							</Row>
 
-  return (
-    <Container>
-      <h1 className="text text-black text-center mt-5 mb-5">
-        Thanks for taking the time to reach out. How can I help you today?
-      </h1>
-      <Form
-        noValidate
-        validated={validated}
-        onSubmit={handleSubmit}
-        // ref={formRef}
-      >
-        <Form.Group className="mb-3" controlId="floatingemail">
-          <Row className="justify-content-center">
-            <Col xs lg="3">
-              <FloatingLabel
-                controlId="floatingname"
-                className="mb-3"
-                label="Name"
-              >
-                <Form.Control type="text" placeholder="Ryan" required />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a name.
-                </Form.Control.Feedback>
-              </FloatingLabel>
-            </Col>
-            <Col xs lg="3">
-              <FloatingLabel
-                controlId="floatingemail"
-                label="Email address"
-                className="mb-3"
-              >
-                <Form.Control
-                  type="email"
-                  placeholder="name@example.com"
-                  required
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a valid email.
-                </Form.Control.Feedback>
-              </FloatingLabel>
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Col xs lg="6">
-              <FloatingLabel
-                controlId="floatingmessage"
-                label="Message"
-                className="mb-3"
-              >
-                <Form.Control
-                  as="textarea"
-                  style={{ minHeight: "200px" }}
-                  required
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a message.
-                </Form.Control.Feedback>
-              </FloatingLabel>
-            </Col>
-          </Row>
-          <Row className="text-center pt-3">
-            <Col>
-              <Button
-                variant="outline-info"
-                className="rounded-pill px-4 py-2"
-                style={{
-                  textDecoration: "none",
-                  fontWeight: 500,
-                  fontSize: "1.1rem",
-                }}
-                type="submit"
-              >
-                Submit
-              </Button>
-            </Col>
-          </Row>
-        </Form.Group>
-      </Form>
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => {
-          setModalShow(false);
-          if (pendingSubmit) {
-            setPendingSubmit(false);
-            navigate("/");
-          }
-        }}
-      />
-    </Container>
-  );
+							<Row className='justify-content-center'>
+								<Col xs lg='6'>
+									<Form.Group
+										className='mb-3'
+										controlId='validationFormik03'>
+										<FloatingLabel
+											controlId='floatingmessage'
+											label='Message'
+											className='mb-3'>
+											<Form.Control
+												as='textarea'
+												name='emailmsg'
+												style={{ minHeight: '200px' }}
+												value={values.emailmsg}
+												onChange={handleChange}
+												isInvalid={!!errors.emailmsg}
+											/>
+											<Form.Control.Feedback type='invalid'>
+												Please enter a message.
+											</Form.Control.Feedback>
+										</FloatingLabel>
+									</Form.Group>
+								</Col>
+							</Row>
+							<Row className='text-center pt-3'>
+								<Col>
+									<Button
+										variant='outline-info'
+										disabled={pendingSubmit}
+										className='rounded-pill px-4 py-2'
+										style={{
+											textDecoration: 'none',
+											fontWeight: 500,
+											fontSize: '1.1rem',
+										}}
+										type='submit'>
+										{pendingSubmit ? (
+											<Spinner
+												animation='border'
+												size='sm'
+											/>
+										) : (
+											'Submit'
+										)}
+									</Button>
+								</Col>
+							</Row>
+						</Form>
+					</Container>
+				</motion.div>
+			)}
+		</Formik>
+	);
 };
 
 export default Contact;
